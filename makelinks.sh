52335d6c9b8404cbbde8d4c67bc0ac22af625581
Pete Young <gnuoy.etep@gmail.com>
2014-10-07 10:43:42 -0500

    ignoring .DS_Store

--- makelinks.sh
+++ makelinks.sh
@@ -11,7 +11,7 @@ cd $dir
 # files to exclude
 #   http://www.bestsolution.at/en/03_Products-nbsp~--nbsp~Projects__13_exclude-nbsp~wildcard-nbsp~matches.html
 #   https://www.gnu.org/software/bash/manual/html_node/Filename-Expansion.html
-GLOBIGNORE=\.git:\.gitignore:*~:*.swp:README.md:dircolors.256dark:makelinks.sh
+GLOBIGNORE=\.git:\.gitignore:*~:*.swp:README.md:dircolors.256dark:makelinks.sh:\.DS_Store
 
 # create symlinks from .dotfiles/ to ~ 
 for file in *; do
