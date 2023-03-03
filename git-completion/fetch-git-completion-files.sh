52f6aff27c04dc9276abe0acb3ad8ed8c2024aa0
Pete Young <gnuoy.etep@gmail.com>
2023-03-02 21:41:21 -0800

    Updated git-completion and git-prompt
    Updated git-completion.bash and git-prompt.sh
    Added zsh completion
    Added script to pull latest from github master

---
+++ git-completion/fetch-git-completion-files.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+curl -O "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh"
+curl -O "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh"
+curl -O "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
