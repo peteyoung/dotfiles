4119965d2fd04e46847ad584a68499afe3133c55
Pete Young <gnuoy.etep@gmail.com>
2023-03-03 14:47:34 -0600

    Setting up for zsh/git integration

--- git-completion/fetch-git-completion-files.sh
+++ git-completion/fetch-git-completion-files.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-curl -O "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh"
 curl -O "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh"
 curl -O "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
+curl -O "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh"
+mv git-completion.zsh _git
