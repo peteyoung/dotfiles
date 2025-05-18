4bbc8df55fe797ec1e1fa0f3ad43f66bb127e3cb
Pete Young <pete@simpli.fi>
2025-05-18 01:54:09 -0500

    Removed git-completion from repo

--- bash_profile
+++ bash_profile
@@ -2,6 +2,3 @@
 if [ -f ~/.bashrc ]; then
     source ~/.bashrc
 fi
-
-# TODO: Move to the appropriate .bash_local file
-#export STM32_PRG_PATH=/Applications/STMicroelectronics/STM32Cube/STM32CubeProgrammer/STM32CubeProgrammer.app/Contents/MacOs/bin
--- git-completion/fetch-git-completion-files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-curl -O "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh"
-curl -O "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
-curl -O "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh"
-mv git-completion.zsh _git
